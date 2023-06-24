8e5d9974e409aa0257affd7474bf36b23a1669c9
Ellen Marie Dash <me@duckie.co>
2023-06-23 22:00:31 -0400

    It can print text, i just need to finish implementing the font

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,25 @@
-// Games made using `agb` are no_std which means you don't have access to the standard
-// rust library. This is because the game boy advance doesn't really have an operating
-// system, so most of the content of the standard library doesn't apply.
-//
 // Provided you haven't disabled it, agb does provide an allocator, so it is possible
 // to use both the `core` and the `alloc` built in crates.
 #![no_std]
 // `agb` defines its own `main` function, so you must declare your game's main function
-// using the #[agb::entry] proc macro. Failing to do so will cause failure in linking
-// which won't be a particularly clear error message.
+// using the #[agb::entry] proc macro. Not doing this will give ugly errors.
 #![no_main]
 // This is required to allow writing tests
 #![cfg_attr(test, feature(custom_test_frameworks))]
 #![cfg_attr(test, reexport_test_harness_main = "test_main")]
 #![cfg_attr(test, test_runner(agb::test_runner::test_runner))]
 
-use agb::{display, syscall};
+use agb::syscall;
+
+/*use agb::{
+    display::tiled::{RegularMap, TileFormat, TileSet, TileSetting, TiledMap, VRamManager},
+    include_background_gfx, rng,
+};*/
+
+pub mod font;
+
+pub mod text;
+use text::{putchar_at, print_at};
 
 // The main function must take 1 arguments and never return. The agb::entry decorator
 // ensures that everything is in order. `agb` will call this after setting up the stack
@@ -23,11 +28,20 @@ use agb::{display, syscall};
 fn main(mut gba: agb::Gba) -> ! {
     let mut bitmap = gba.display.video.bitmap3();
 
-    for x in 0..display::WIDTH {
-        let y = syscall::sqrt(x << 6);
-        let y = (display::HEIGHT - y).clamp(0, display::HEIGHT - 1);
+    putchar_at(&mut bitmap, 0, 0, '!');
+    putchar_at(&mut bitmap, 0, 1, 'A');
+    print_at(&mut bitmap, 1, 0, "Hello, world!");
+    /*putchar_at(&mut bitmap, 0, 0, 'H');
+    putchar_at(&mut bitmap, 0, 1, 'e');
+    putchar_at(&mut bitmap, 0, 0, 'l');
+    putchar_at(&mut bitmap, 0, 0, 'l');
+    putchar_at(&mut bitmap, 0, 0, 'o');*/
+    print_at(&mut bitmap, 2, 0, " !\"#$%&;()*+,-./0123456789:;<=>?@\nABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~");
+
+    /*for x in 0..display::WIDTH {
+        let y = x * display::HEIGHT / display::WIDTH;
         bitmap.draw_point(x, y, 0x001F);
-    }
+    }*/
 
     loop {
         syscall::halt();
---
+++ src/text.rs
@@ -0,0 +1,55 @@
+use agb::display::bitmap3::Bitmap3;
+use agb::display;
+
+use super::font::{CHARS, CHAR_IDX_OFFSET};
+
+const ROWS: i32 = (display::HEIGHT / 8) as i32;
+const COLS: i32 = (display::WIDTH / 8) as i32;
+
+pub fn putchar_at(bitmap: &mut Bitmap3, row: i32, col: i32, letter: char) {
+    let chr = CHARS[letter as usize - CHAR_IDX_OFFSET];
+    let x = col * 8;
+    let y = row * 8;
+    for xoff in 0..8 {
+        for yoff in 0..8 {
+            if chr[((yoff * 8) + xoff) as usize] != 0 {
+                bitmap.draw_point(x + xoff, y + yoff, 0x001F);
+            }
+            //let pixel = if chr[((yoff * 8) + xoff) as usize] == 0 { 0x0000 } else { 0x001F };
+            //bitmap.draw_point(x as i32 + xoff, y as i32 + yoff, pixel);
+        }
+    }
+}
+
+fn scroll_text(_bitmap: &mut Bitmap3) {
+    // TODO: Implement me.
+}
+
+pub fn print_at(bitmap: &mut Bitmap3, row: i32, col: i32, text: &str) {
+    let mut row = row;
+    let mut col = col;
+    for letter in text.chars() {
+        if letter == '\r' {
+            col = 0;
+            continue;
+        }
+
+        if letter == '\n' || col >= COLS {
+            row += 1;
+            col = 0;
+
+            // This lets us avoid having 22 empty things in font::CHARS.
+            if letter == '\n' {
+                continue;
+            }
+        }
+
+        if row >= ROWS {
+            scroll_text(bitmap);
+        }
+
+        putchar_at(bitmap, row, col, letter);
+
+        col += 1;
+    }
+}
